5ac0da8fc48b73dfdcae50d923220a3378409b39
nathancoblentz <nathancoblentz@gmail.com>
2017-10-09 21:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,3 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 happyHolidays()
-
-function happyHolidaysTo (name) {
-  console.log(Happy holidays ${name}!')
-}
-
-happyHolidaysTo('you')
