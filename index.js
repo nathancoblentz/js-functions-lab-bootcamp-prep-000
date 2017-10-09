57475f4764cce4250425513f65bcba04196735f1
nathancoblentz <nathancoblentz@gmail.com>
2017-10-09 21:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 happyHolidays()
+
+
+function happyHolidaysTo (name) {
+  console.log('Happy holidays ${name}!')
+}
+
+happyHolidaysTo('you')
+
